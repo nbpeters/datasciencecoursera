e52845e42493a463f7a33495b55befa54ddb062f
nbpeters <nbpeters02@gmail.com>
2026-05-27 22:09:20 -0400

    Fixed comments to follow coding standards suggested by lecturer

--- Scripts/Practice with Functions.R
+++ Scripts/Practice with Functions.R
@@ -15,11 +15,15 @@ above <- function(x, n = 10) {
 }
 
 columnMeans <- function(y, removeNA = TRUE){
-  nc <- ncol(y) #set number of columns in data frame
-  means <- numeric(nc) # Numeric vector that will hold the resulting means from each column
+  #set number of columns in data frame
+  nc <- ncol(y)
+  # Numeric vector that will hold the resulting means from each column
+  means <- numeric(nc) 
   for(i in 1:nc){
-    means[i] <- mean(y[,i], na.rm = removeNA) #loop through dataframe and store mean in means object
+    #loop through dataframe and store mean in means object
+    means[i] <- mean(y[,i], na.rm = removeNA) 
   }
-  #Still need to return something from this function so time to call the means object
+  #Still need to return something from this function 
+  #so time to call the means object
   means
 }
