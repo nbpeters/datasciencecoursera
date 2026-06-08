e9d1ed490c1e6001a09dc9434032c6a158a6e7b9
nbpeters <nbpeters02@gmail.com>
2026-06-08 13:48:32 -0400

    erm...

--- Scripts/Module 2 Function Scripts.R
+++ Scripts/Module 2 Function Scripts.R
@@ -1,10 +1,11 @@
 #-----------------Coursera Module 2 Assignment----------------
 # Function 1 ----- pollutantmean -----------------------------
 pollutantmean <- function(dataDirectory, pollutant, id = 1:332){
-  #setwd(dataDirectory)
+  #creating an empty numeric vector for pollutant data entries to fill for later
+  #mean calculation
   tableVec <- numeric()
   for(i in id){
-    #settting id to character vector matching monitor file
+    #creating a character vector matching monitor file for path finding
     if(i < 10){
       tablename <- paste("00",i,".csv", sep = "")
     }
@@ -14,16 +15,21 @@ pollutantmean <- function(dataDirectory, pollutant, id = 1:332){
     else {
       tablename <- paste(i,".csv", sep = "")
     }
-    tablepath <- file.path(dataDirectory, tablename)
-    #assigning current monitor table to an object for summation before mean
-    looptable <- read.csv(tablepath)
-    #summing non-NA values into total tableSum object
-    tableVec <- c(tableVec, looptable[[pollutant]]) 
     
-    # rows <- rows + sum(!is.na(looptable[[pollutant]]))
+    #creating a path to the monitor file to be executed within the loop instead
+    #of relying on changing the global working directory to the Data folder
+    tablepath <- file.path(dataDirectory, tablename)
+    
+    #assigning current monitor table to an object for summation into a single vector
+    #before mean function
+    looptable <- read.csv(tablepath)
+    
+    #summing non-NA values into a total tableSum object/vector
+    tableVec <- c(tableVec, looptable[[pollutant]]) 
   }
   mean(tableVec, na.rm = TRUE)
-  #tableSum/rows
+  #could set return here for good practice but since mean is final expression
+  #this alone should suffice
 }
 
     
